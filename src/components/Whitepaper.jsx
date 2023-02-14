6a67957a4abc2a240b6b541d1b8240fb9ad419f3
jbenedicto20.jbb@gmail.com <JBenedicto13>
2023-02-14 21:05:44 +0800

    Add whitepaper

--- src/components/Whitepaper.jsx
+++ src/components/Whitepaper.jsx
@@ -1,10 +1,49 @@
-import '../App.css'
+import '../styles/whitepaper.css';
 
 function Whitepaper() {
 
   return (
-    <div className="App">
-        <p>Whitepaper Page</p>
+    <div className="whitepaper">
+      <div className='header'>
+        <h2>THE WHITEPAPER</h2>
+      </div>
+      <div className='content'>
+        <div className='content-top'>
+          <p>Aside from regular cute PFP or Web3 Pet, our project is to have interactive story creation. It is to inspire aspiring writers to professionals. All welcome to share their creativeness. Their imaginations. We know that behind successful projects, there are writers who are doing their best to make the project eye catcher. It's time to shine for everyone!</p>
+        </div>
+        <hr></hr>
+        <div className='content-main'>
+          <h3>1st Phase</h3>
+          <ul>
+            <li>Community Build-up</li>
+            <li>Marketing</li>
+            <li>Whitelisting</li>
+            <li>Preparation of Genes</li>
+            <li>Minting of 5,555 different EGGZUKIs</li>
+            <li>Chapter 1</li>
+          </ul>
+
+          <h3>2nd Phase</h3>
+          <ul>
+            <li>Events for the Interactive Continuation of Chapters (Rewards)</li>
+            <li>Preparation of (TBD) Arcanes - Limited Edition</li>
+            <li>Preparation of Fusion option of EGGZUKI and the items</li>
+            <li>Release of Common to Arcane Items</li>
+          </ul>
+
+          <h3>3rd Phase</h3>
+          <ul>
+            <li>Events for the Interactive Continuation of Chapters (Rewards)</li>
+            <li>Release of EGG Token and airdrop to EGGZ Holders</li>
+          </ul>
+
+          <h3>4th Phase</h3>
+          <ul>
+            <li>Will it be horrible death? or a success to our protagonist? There is one thing to find out. Stay tuned.</li>
+          </ul>
+        </div>
+        <hr></hr>
+      </div>
     </div>
   )
 }
