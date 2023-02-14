417c2f4c48cbacc7ee7bec33e65009eaf2e6e64c
jbenedicto20.jbb@gmail.com <JBenedicto13>
2023-02-14 22:01:23 +0800

    Add lore with temporary map

--- src/components/Lore.jsx
+++ src/components/Lore.jsx
@@ -1,10 +1,51 @@
-import '../App.css'
+import '../App.css';
+import '../styles/lore.css';
+import React, { useState } from 'react';
 
 function Lore() {
 
+  const [selectedChapter, setSelectedChapter] = useState("");
+
+  function getSelectedChapter(val) {
+    if (val === "Chapter 1") {
+      setSelectedChapter("Chapter 1")
+    } else if (val === "Chapter 2") {
+      setSelectedChapter("Chapter 2")
+    }
+  }
+
   return (
-    <div className="App">
-        <p>Lore Page</p>
+    <div className="lore">
+      <div className='content'>
+        <img src='https://img.freepik.com/free-vector/map-pirate-island-treasure_23-2147629202.jpg?w=740&t=st=1676380322~exp=1676380922~hmac=8e8f4f361cad1e29380f0ca4152583ee054bab909dbb4b0bb94f14c4a3d6d427' useMap="#image-map"></img>
+        
+        <div onClick={()=>getSelectedChapter("Chapter 1")} className='areas' id='area1' data-bs-toggle="modal" data-bs-target="#staticBackdrop"></div>
+        <div onClick={()=>getSelectedChapter("Chapter 2")} className='areas' id='area2' data-bs-toggle="modal" data-bs-target="#staticBackdrop"></div>
+
+        <map name="image-map">
+            <area target="_blank" alt="Chapter 1" title="Chapter 1" href="https://youtube.com" coords="457,510,12" shape="circle"/>
+            <area target="_blank" alt="Chapter 2" title="Chapter 2" href="https://twitter.com" coords="294,470,13" shape="circle"/>
+        </map>
+
+      </div>
+
+      <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="staticBackdropLabel">{selectedChapter}</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              {selectedChapter}
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Understood</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,28 +18,28 @@ const Navbar = () => {
     },[document_title]);
 
     return (
-    <nav class="navbar navbar-expand-lg sticky-top">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="/home">Welcome to Eggzuki!</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg sticky-top">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/home">Welcome to Eggzuki!</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="/home">Home</a>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="/home">Home</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/lore">Lore</a>
+              <li className="nav-item">
+                <a className="nav-link" href="/lore">Lore</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/whitepaper">Whitepaper</a>
+              <li className="nav-item">
+                <a className="nav-link" href="/whitepaper">Whitepaper</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/mint">Mint Now</a>
+              <li className="nav-item">
+                <a className="nav-link" href="/mint">Mint Now</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/fusion">Fusion</a>
+              <li className="nav-item">
+                <a className="nav-link" href="/fusion">Fusion</a>
               </li>
             </ul>
           </div>
