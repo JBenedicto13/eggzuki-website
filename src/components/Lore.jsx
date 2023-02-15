9916644414575ef366e59939b22388e4ad09622d
jbenedicto20.jbb@gmail.com <JBenedicto13>
2023-02-15 20:15:42 +0800

    Add image map with react-image-mapper library

--- src/components/Lore.jsx
+++ src/components/Lore.jsx
@@ -2,36 +2,61 @@ import '../App.css';
 import '../styles/lore.css';
 import React, { useState } from 'react';
 import { ChapterData } from './ChapterData';
+import ImageMapper from "react-image-mapper";
 
 function Lore() {
 
   const [selectedChapter, setSelectedChapter] = useState("");
 
-  function getSelectedChapter(val) {
-    if (val === "Chapter 1") {
+  const getSelectedChapter = (area) => {
+    if (area.name === "Chapter 1") {
       setSelectedChapter("Chapter 1")
-    } else if (val === "Chapter 2") {
+      document.getElementById("btnShowChapter").click()
+    } else if (area.name === "Chapter 2") {
       setSelectedChapter("Chapter 2")
+      document.getElementById("btnShowChapter").click()
     }
   }
+
+  var MAP = {
+    name: "Loremap",
+    areas: [
+    {
+        name: "Chapter 1",
+        shape: "circle",
+        coords: [456,511,12],
+        preFillColor: "green",
+        lineWidth: 3,
+        fillColor: "#0000ff",
+    },
+    {
+        name: "Chapter 2",
+        shape: "circle",
+        coords: [569,283,23],
+        preFillColor: "pink",
+        lineWidth: 3,
+        strokeColor: "#0000ff",
+    },
+    ]
+  }
+
+  var URL = "https://img.freepik.com/free-vector/map-pirate-island-treasure_23-2147629202.jpg?w=740&t=st=1676380322~exp=1676380922~hmac=8e8f4f361cad1e29380f0ca4152583ee054bab909dbb4b0bb94f14c4a3d6d427";
 
   return (
     <div className="lore">
       <div className='content'>
-        <img src='https://img.freepik.com/free-vector/map-pirate-island-treasure_23-2147629202.jpg?w=740&t=st=1676380322~exp=1676380922~hmac=8e8f4f361cad1e29380f0ca4152583ee054bab909dbb4b0bb94f14c4a3d6d427' useMap="#image-map"></img>
-        
-        <div onClick={()=>getSelectedChapter("Chapter 1")} className='areas' id='area1' data-bs-toggle="modal" data-bs-target="#staticBackdrop"></div>
-        <div onClick={()=>getSelectedChapter("Chapter 2")} className='areas' id='area2' data-bs-toggle="modal" data-bs-target="#staticBackdrop"></div>
-
-        <map name="image-map">
-            <area target="_blank" alt="Chapter 1" title="Chapter 1" href="https://youtube.com" coords="457,510,12" shape="circle"/>
-            <area target="_blank" alt="Chapter 2" title="Chapter 2" href="https://twitter.com" coords="294,470,13" shape="circle"/>
-        </map>
-
+        <ImageMapper 
+          src={URL} 
+          map={MAP}
+          onClick={(area) => getSelectedChapter(area)}
+        />
+        <button id='btnShowChapter' hidden type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  ShowChapter
+</button>
       </div>
 
       <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div className="modal-dialog modal-xl">
+        <div className="modal-dialog modal-lg modal-dialog-scrollable">
           {ChapterData.map((item, key) => {
             if (item.chapter === selectedChapter) {
               return (
